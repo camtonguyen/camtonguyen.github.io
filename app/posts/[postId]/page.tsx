0702133e3e52441a1e0d4dcc28739b6834673a60
Cam To <nguyenthicamto@gmail.com>
2024-12-01 14:04:12 +0700

    upgrade next and fix async func get Post

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,58 +1,57 @@
-import formatDate from "@/lib/formatDate";
-import { getPostData, getSortedPostsData } from "@/lib/posts"
-import Link from "next/link";
-import { notFound } from "next/navigation";
+import formatDate from '@/lib/formatDate';
+import { getPostData, getSortedPostsData } from '@/lib/posts';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
-export function generateStaticParams() {
-  const posts = getSortedPostsData()
+export async function generateStaticParams() {
+  const posts = await getSortedPostsData();
 
   return posts.map((post) => ({
-      postId: post.id
-  }))
+    postId: post.id,
+  }));
 }
 
-export function generateMetadata({ params }: { params: { postId: string } }) {
+export async function generateMetadata(props: { params: { postId: string } }) {
+  const { params } = props;
+  const { postId } = await params;
 
-  const posts = getSortedPostsData()
-  const { postId } = params
+  const posts = await getSortedPostsData();
 
-  const post = posts.find(post => post.id === postId)
+  const post = posts.find((post) => post.id === postId);
 
   if (!post) {
-      return {
-          title: 'Post Not Found'
-      }
+    return {
+      title: 'Post Not Found',
+    };
   }
 
   return {
-      title: post.title,
-  }
+    title: post.title,
+  };
 }
 
-export default async function Post({ params }: { params: { postId: string } }) {
+export default async function Post(props: { params: { postId: string } }) {
+  const { params } = props;
+  const { postId } = await params;
 
-  const posts = getSortedPostsData()
-  const { postId } = params
+  const posts = await getSortedPostsData();
 
-  if (!posts.find(post => post.id === postId)) notFound()
+  if (!posts.find((post) => post.id === postId)) notFound();
 
-  const { title, date, contentHtml } = await getPostData(postId)
+  const { title, date, contentHtml } = await getPostData(postId);
 
-  const publishedDate = formatDate(date)
+  const publishedDate = formatDate(date);
 
   return (
-      <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-          <h1 className="text-3xl mt-4 mb-0">{title}</h1>
-          <p className="mt-2 text-sm">
-              {publishedDate}
-          </p>
-          <article>
-              <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
-              <p>
-                  <Link href="/">← Back to home</Link>
-              </p>
-          </article>
-      </main>
-  )
+    <main className='px-6 prose prose-xl prose-slate dark:prose-invert mx-auto'>
+      <h1 className='text-3xl mt-4 mb-0'>{title}</h1>
+      <p className='mt-2 text-sm'>{publishedDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          <Link href='/'>← Back to home</Link>
+        </p>
+      </article>
+    </main>
+  );
 }
-
